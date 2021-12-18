5d95324f4dc92bb371cf1df3417959d429f2ec93
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-12-18 18:59:50 -0300

    feat: Initial template structure

---
+++ bin/ocaml_starter.ml
@@ -0,0 +1,16 @@
+open Starter_lib
+
+let usage = "USAGE: esy x ocaml_starter <dividend> <divisor>"
+
+let () =
+  match Array.to_list Sys.argv with
+  | [ _program; a; b ] ->
+    let a = float_of_string a in
+    let b = float_of_string b in
+
+    Math.safe_div a b
+    |> Option.fold ~none:"Cannot divide by 0" ~some:(Printf.sprintf "Result of division is %.1f")
+    |> print_endline
+  | _ ->
+    prerr_endline usage;
+    exit 1
---
+++ lib/math.ml
@@ -0,0 +1,1 @@
+let safe_div a b = if b = 0.0 then None else Some (a /. b)
